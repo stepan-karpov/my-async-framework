5e52b72b26860540371d8042dcd96e0788cd59e1
stepan-karpov <stepan.karpov@list.ru>
2025-11-30 16:29:25 +0000

    remove .sandbox

--- .sandbox/learning-c++/valgrind.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-  int* x_ptr = new int(5);
-
-  delete x_ptr;
-}
--- .sandbox/learning-concurrency/hardware-concurrency.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <thread>
-
-int main() {
-  auto n = std::thread::hardware_concurrency();
-  std::cout << "std::thread::hardware_concurrency() = " << n << "\n";
-
-  // std::thread::hardware_concurrency() = 10
-}
